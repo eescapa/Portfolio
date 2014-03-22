e128d90fb8e770a5e521682b1009303abb9be85b
Elisa Asile <elisaeo10@gmail.com>
2014-03-22 16:30:57 -0400

    Nav bar, added Footer

--- scripts/panoptic.js
+++ scripts/panoptic.js
@@ -1,12 +1,25 @@
 $(document).ready(function () {
 
-
-	/* Toggle slide down when nav h3 clicked */
-	$mainNav = $('nav h3');
+	/* Make nav stick to top */
+	var stickyTop = $('nav').offset().top; // returns number 
+ 
+	$(window).scroll(function(){ // scroll event  
 	
-	$mainNav.click(function() {
-		$('ul.subNav li').slideToggle('slow');
+		/* Only call when non-Responsive */
+		if($(document).width() > 567){ 
+		
+			$navWidth = $('nav').width();
+			var windowTop = $(window).scrollTop(); // returns number
+			
+			//if nav top is touching the top of the window, make nav stick to it
+			if (stickyTop < windowTop) {
+				$('nav').css({ position: 'fixed', top: 0, width: $navWidth });
+			}
+			else {
+				$('nav').css({ position: 'static', width:'25%' });
+			}
+		}		
 	});
 
-
 });
+
